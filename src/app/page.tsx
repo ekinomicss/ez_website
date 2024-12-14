f42ee4b0b9f8cff7cac2e797d3a22699c762c5ba
ekinomicss <zorerekin@gmail.com>
2024-12-14 10:27:38 -0500

    fix tsc issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import Home from '../components/HomeBox'
 
-import Link from 'next/link';
-// import { getSortedPostsData } from '../utils/markdownToHtml';
-import { useEffect, useState } from 'react';
-import Home from '../components/HomeBox';
-
-
-export default Home;
+export default function Page() {
+    return <Home />
+}
