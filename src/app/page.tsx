bf200d403512a5676f0d50db59bd689e00a7193a
ekinomicss <zorerekin@gmail.com>
2024-12-14 10:22:10 -0500

    tsx changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 // import { getSortedPostsData } from '../utils/markdownToHtml';
 import { useEffect, useState } from 'react';
-import Home from '../components/Home';
+import Home from '../components/HomeBox';
 
 
 export default Home;
